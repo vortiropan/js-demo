809d2eaea7a1a8aa407fcafe0bda7c018f0c503d
michael <vortiropan@gmail.com>
2024-06-10 06:20:11 +0300

    createFunc buyProduct

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-// const a = 5
-// console.log(a < 2 ?
-//             a > 4 ?
-//             'Yes' : a <= 4 ?
-//              'No': '!!!' : '???')
+function getCredit(agreement, age, yesWorks){
+    let summCredit = 0;
+    if (age >= 24 && yesWorks && agreement){
+        return summCredit = 500;
+    } else if (age >= 24 && agreement){
+        return summCredit = 100;
+    } else{
+        return summCredit = false;
+    }
+}
 
-console.log(true && 'd')
+function buyProduct(priceProduct, agreement, age, cash, yesWorks){
+    if(cash >= priceProduct){
+        return true;
+    } else{
+        return getCredit(agreement, age, yesWorks) + cash >= priceProduct ? true : false;
+    }
+    
+}
+
+console.log(buyProduct(2000, true, 24, 1500, true));
